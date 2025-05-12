fad48ac5ea4af9bb8e2ff5aedc1536f9d1ed3a98
Gelosora <150021337+Gelosora@users.noreply.github.com>
2025-05-13 00:31:53 +0800

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resource/sass/app.scss','resource/js./app.js'])
+</head>
+<body class="bg-dark">
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+    </main>
+    </div>
+    
+</body>
+</html>
